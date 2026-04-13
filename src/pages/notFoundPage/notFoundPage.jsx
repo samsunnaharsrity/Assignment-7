76a328e75856c310b7231f26e3862866e0a7e690
Samsunnahar Srity <samsunnaharsrity@gmail.com>
2026-04-14 03:20:43 +0600

    error page handle

--- src/pages/notFoundPage/notFoundPage.jsx
+++ src/pages/notFoundPage/notFoundPage.jsx
@@ -8,7 +8,7 @@ const NotFoundPage = () => {
   return (
     <div className='flex flex-col items-center justify-center min-h-screen text-center'>
       <h1 className='text-3xl font-bold'>404</h1>
-      <p>Oops! Page not found.</p>
+      <p>Oops! This page not found.</p>
       <Link to="/">
       <button className='btn bg-[#244d3f] text-white mt-3'>Go Back Home</button>
       </Link>
