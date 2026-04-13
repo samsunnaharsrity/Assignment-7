733b8aaa9f815365632016dd87dd2431fc91e6f8
Samsunnahar Srity <samsunnaharsrity@gmail.com>
2026-04-14 03:23:29 +0600

    navbar done

--- src/components/shared/navbar.jsx
+++ src/components/shared/navbar.jsx
@@ -25,14 +25,7 @@ const Navbar = () => {
                     </button>
 
                     <button className='px-2 py-2'>
-                        <NavLink to={"/timeLine"}
-                        className={({isActive})=>`
-                        flex items-center gap-1
-                        ${isActive? 'bg-[#244d3f] text-white px-2 py-1 font-bold' : ''}
-                        `}
-                        >
-                        <img className='w-[12px] h-[12px]' src={clickImg} alt="clickImg" />
-                        Timeline</NavLink>
+
                     </button>
 
                     <button className='px-2 py-2'>
