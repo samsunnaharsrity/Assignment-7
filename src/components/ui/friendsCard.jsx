86f9157ce7cb57923e9ecbbc302053d05eeeb878
Samsunnahar Srity <samsunnaharsrity@gmail.com>
2026-04-15 02:34:46 +0600

    friend card style done

--- src/components/ui/friendsCard.jsx
+++ src/components/ui/friendsCard.jsx
@@ -11,31 +11,7 @@ const FriendsCard = ({index ,friend}) => {
                 <div><p className='font-bold text-md'>{friend.name}</p></div>
                 <div><p className='text-[#64748b] text-[10px]'>{friend.lastContact}</p></div>
 
-                {/* tag btn list */}
-                <div>
-                    <button className='flex gap-2 w-full justify-center' >
-                    {
-                        friend.tags.map((tag,index)=>(
-                            <span key={index} className='bg-green-200 rounded-3xl text-[8px] px-2 py-1'>
-                                {tag}
-                            </span>
-                        ))
-                    }
-                    </button>
-                </div>
 
-                {/* status btn list */}
-                <div>
-                    <button className={` text-white rounded-3xl text-[10px] px-2 py-1
-                        ${
-                            friend.status === "Almost Due" ? "bg-[#efad44]":
-                            friend.status === "Overdue" ? "bg-[#ef4444]":
-                            'bg-[#244d3f]'
-                        }
-                        `}>
-                        {friend.status}
-                    </button>
-                </div>
 
             </div>            
         </div> 
